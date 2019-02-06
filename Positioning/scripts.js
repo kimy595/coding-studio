cf4c79ef8040499ed3bb65ddf8a776e45e732395
Yousun Kim <kimy595@newschool.edu>
2019-02-06 13:22:38 -0500

    scripts

---
+++ Positioning/scripts.js
@@ -0,0 +1,10 @@
+(function(inview){
+inview('.absolute_window')
+.on('enter', function(el) {
+    console.log('--interview message absolutely positioned--',el);
+    el,classNmae ='active'
+});
+
+
+var script = 'script';
+})(inview);
